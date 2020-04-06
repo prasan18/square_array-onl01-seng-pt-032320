351895235205ec2d675d06f373c00f816c912dc2
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-06 00:02:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+def square_array(numbers)
+array.each do |numbers|array << numbers ** 2
+array
+
 end
+def square_array(new_numbers) 
+new_numbers = [9,10,16,25]
+array.each do |new_numbers|array << numbers ** 2
+array
+end
+
+ end
